c6e95d5664c00a494c27c12a01ba80ac97593b34
Roger Herikstad <roger.herikstad@gmail.com>
2014-01-30 07:59:46 +0800

    Added functions for computing entropy on binned data

---
+++ src/binned.jl
@@ -0,0 +1,35 @@
+using Stats
+
+function entropy{T<:Integer}(x::Array{T,1})
+    P = x/sum(x) #convert to probabilitiy
+    return -dot(P,log2(P + (P.==0)))
+end
+
+#functions operating on tress
+function entropy(X::Dict{Int64, Dict{Int64, Dict{Int64,Int64}}})
+    E = 0
+    N = 0
+    for (k,v) in X
+        ee,n = entropy(v)
+        E += ee*n
+        N += n
+    end
+    return E/N,N
+end
+
+function entropy(X::Dict{Int64, Dict{Int64,Int64}})
+    E = 0
+    N = 0
+    for (k,v) in X
+        ee,n = entropy(v)
+        E += ee*n
+        N += n
+    end
+    return E/N,N
+end
+
+function entropy(X::Dict{Int64,Int64})
+    N = sum(values(X))
+    P = float(map(v -> v/N, values(X)))
+    return (-sum(P.*log2(P + float(P.==0))),N)
+end
