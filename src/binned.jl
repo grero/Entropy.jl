0d0db6f531cb7d38659ecf601cdf5a7522b9d0d1
Roger Herikstad <roger.herikstad@gmail.com>
2014-02-20 08:23:07 +0800

    Added function to compute information of a confusion matrix

--- src/binned.jl
+++ src/binned.jl
@@ -8,6 +8,18 @@ function entropy{T<:Integer}(x::Array{T,1})
     return -dot(P,log2(P + (P.==0)))
 end
 
+function entropy{T<:Real}(Nc::Array{T,2})
+    N = sum(Nc[:])
+    H1 = log2(Nc + (Nc.==0))
+    H2 = log2(sum(Nc,1))
+    H3 = log2(sum(Nc,2))
+    H4 = log2(N)
+    H = Nc.*(broadcast(-,broadcast(-, H1, H2),H3) + H4)
+    H[isnan(H)] = 0
+    H = 1/N*sum(sum(H))
+    return H
+end
+
 #functions operating on tress
 function entropy(X::Dict{Int64, Dict{Int64, Dict{Int64,Int64}}})
     E = 0
