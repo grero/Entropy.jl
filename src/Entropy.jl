126344569d886986e0ede56120ee6847acf63e02
Roger Herikstad <roger.herikstad@gmail.com>
2015-01-15 20:23:48 +0800

    Various updates

--- src/Entropy.jl
+++ src/Entropy.jl
@@ -1,9 +1,19 @@
 module Entropy
-
+using HDF5, JLD
+include("types.jl")
+include("nsb2.jl")
+include("utils.jl")
+include("bias.jl")
 include("hist.jl")
 using .Histograms
 include("binless.jl")
 include("binned.jl")
+include("algorithms.jl")
+include("process.jl")
+import GUICheck
+if GUICheck.hasgui()
+	include("plot.jl")
+end
 
 export Hist2d
 
