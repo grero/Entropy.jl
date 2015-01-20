a05a6a2951e80536e38d747d9884105d5b0b51ef
Roger Herikstad <roger.herikstad@gmail.com>
2015-01-20 14:18:56 +0800

    Added optional alignment keyword

--- src/process.jl
+++ src/process.jl
@@ -2,7 +2,7 @@ import Stimulus
 import Spiketrains
 import JLD
 
-function processTemporalEntropy(sptrains::Dict, bins::Array{Float64,1}, trials::Union(Array{Stimulus.Trial,1},Array{Stimulus.SaccadeTrial,1}),ncombos::Integer, word_size::Integer;skipMissing::Bool=false)
+function processTemporalEntropy(sptrains::Dict, bins::Array{Float64,1}, trials::Union(Array{Stimulus.Trial,1},Array{Stimulus.SaccadeTrial,1}),ncombos::Integer, word_size::Integer;skipMissing::Bool=false,alignment::Symbol=:target)
 	cells = Spiketrains.sortCells(sptrains)
 	tmin = minimum(bins)
 	tmax = maximum(bins)
@@ -18,7 +18,7 @@ function processTemporalEntropy(sptrains::Dict, bins::Array{Float64,1}, trials::
                     isfile(fname) && println("No data found. Computing...")
                     S = sptrains[_cell] 
                     println("Processing cell $_cell")
-					spikes = Spiketrains.getTrialRaster(S,trials, :target,tmin,tmax)
+					spikes = Spiketrains.getTrialRaster(S,trials, alignment,tmin,tmax)
 					psth = Spiketrains.getTrialSpikeCount(spikes, bins)
 					GTE = Entropy.GroupedTemporalEntropy(int(psth.counts),bins,trial_labels[1:psth.ntrials],word_size)
 					JLD.save(fname, {"GTE"=>GTE})
