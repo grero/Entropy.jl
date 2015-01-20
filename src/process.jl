57e81cc8d02f9666cdab4157048e2d9d13d5cbeb
Roger Herikstad <roger.herikstad@gmail.com>
2015-01-20 16:45:05 +0800

    Added more generic process function

--- src/process.jl
+++ src/process.jl
@@ -28,3 +28,30 @@ function processTemporalEntropy(sptrains::Dict, bins::Array{Float64,1}, trials::
 		end,cells)
 	return D
 end
+
+function process{T<:GroupedShannonEntropy}(::Type{T},sptrains::Dict, bins::Array{Float64,1}, trials::Union(Array{Stimulus.Trial,1},Array{Stimulus.SaccadeTrial,1}),ncombos::Integer, word_size::Integer;skipMissing::Bool=false,alignment::Symbol=:target)
+	cells = Spiketrains.sortCells(sptrains)
+	tmin = minimum(bins)
+	tmax = maximum(bins)
+	trial_labels = Stimulus.getTrialLocationLabel(trials,:target)
+    D = pmap( _cell -> begin
+            fname = string(_cell,filename(T))
+            if isfile(fname)
+                println("File already exists. Skipping...")
+			else 
+                if skipMissing
+                    println("Skipping missing data..")
+                else
+                    isfile(fname) && println("No data found. Computing...")
+                    S = sptrains[_cell] 
+                    println("Processing cell $_cell")
+					spikes = Spiketrains.getTrialRaster(S,trials, alignment,tmin,tmax)
+					psth = Spiketrains.getTrialSpikeCount(spikes, bins)
+					GTE = T(int(psth.counts),bins,trial_labels[1:psth.ntrials],word_size)
+					JLD.save(fname, {varname(T)=>GTE})
+				end
+			end
+			fname
+		end,cells)
+	return D
+end
