de8055bf3d7b7c7281590e773c8b191df81e6a1f
Roger Herikstad <roger.herikstad@gmail.com>
2015-01-20 14:20:32 +0800

    Made some changed to bias calculations

--- src/bias.jl
+++ src/bias.jl
@@ -1,5 +1,8 @@
 #collection of bias correction routines
 
-function ma(K, N)
-    return (K-1)/(2*N)
+ma(K,N) = ma(K,N,2.0)
+function ma(K, N, base)
+    return (K-1)/(2*N*log(base))
 end
+
+blank(args...) = 0
